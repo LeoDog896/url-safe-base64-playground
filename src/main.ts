44fc73d131628df118c64f9125a5e30d6be4caa5
Tristan F <LeoDog896@hotmail.com>
2023-06-25 20:09:52 -0400

    feat: initial playground

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import {
+  encode, decode, trim,
+  isBase64, isUrlSafeBase64
+} from 'url-safe-base64'
 import './style.css'
 
-const encodeInput = document.querySelector("textarea")
-const encodeOutput = document.querySelector("#encodeOutput")
+const encodeInput = document.querySelector("textarea")!
+const encodeOutput = document.querySelector("#encodeOutput")!
 
-const decodeInput = document.querySelector("input")
-const decodeOutput = document.querySelector("#decodeOutput")
+const decodeInput = document.querySelector("input")!
+const decodeOutput = document.querySelector("#decodeOutput")!
 
+decodeInput.addEventListener("input", () => {
+  const value = decodeInput.value
+  if (isBase64(value)) {
+    if (isUrlSafeBase64(value)) {
+      decodeOutput.innerHTML = atob(decode(value))
+    } else {
+      decodeOutput.innerHTML = "Base64 is not url safe"
+    }
+  } else {
+    decodeOutput.innerHTML = "Not base64"
+  }
+})
+
+encodeInput.addEventListener("input", () => {
+  encodeOutput.textContent = encode(btoa(trim(encodeInput.value)))
+});
